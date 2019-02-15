ccc3411e878169ed026228030b100361bf25d75e
rhodso <rtrhodes1997@gmail.com>
2019-02-15 13:03:52 +0000

    /Hopefully/ should now respond to commands

--- Python/UtilitiesBot.py
+++ Python/UtilitiesBot.py
@@ -1,11 +1,15 @@
+#Imports
 import discord
 import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import datetime
+import random
 
-prefix = '!'
+#Set prefix character
+prefix = "!"
+total = 0
 
 #Def log method
 def log(Message):
@@ -23,24 +27,31 @@ client = Bot(description='', command_prefix=prefix, pm_help = False)
 #Startup sequence
 @client.event
 async def on_ready():
-    print("")
     log('Initialising...')
     log('Logged in as '+client.user.name+' (ID:'+client.user.id+') \n Connected to '+str(len(client.servers))+' servers and '+str(len(set(client.get_all_members())))+' users')
     log('')
     log('Current Discord.py Version: {} \n Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-    log('Ready!\n\n')
+    log('Ready!')
     return await client.change_presence(game=discord.Game(name='')) 
 
 #Commands
 @client.event
 async def on_message(message):
-    if(message == prefix + "help"):
-        log('Running help command...')
-        await client.send_message(client.get_channel(message.channel.id), 'Here\'s some text')
+    if(message.content[:1] == prefix):
 
-    elif(message == prefix + "ping"):
-        log('Running ping command...')
-        await client.send_message(client.get_channel(message.channel.id), 'Pong!')
+        #Help command
+        if(message.content == (prefix + "help")):
+            log("Running help command...")
+            await client.send_message(client.get_channel(message.channel.id),'Here\'s some text')
+
+        #Ping command
+        if(message.content == (prefix + "ping")):
+            log("Running ping command...")
+            await client.send_message(client.get_channel(message.channel.id), 'Pong!')
+
+    else:
+        #Message is not a command, ignore
+        pass
 
 #Run bot
 client.run(str(getToken()))
