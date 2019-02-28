4bdd5ef40356710adbe5390a6e14b06d0bc75717
rhodso <rtrhodes1997@gmail.com>
2019-02-28 14:56:59 +0000

    Added Jython

--- Java/UtilitiesBot/src/commands.java
+++ Java/UtilitiesBot/src/commands.java
@@ -1,8 +1,13 @@
+import java.util.Random;
+
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.python.util.PythonInterpreter;
 
 public class commands implements MessageCreateListener {
     private String prefix;
+    private Random rng = new Random();
+    PythonInterpreter interpreter = new PythonInterpreter();
 
     public commands(String prefix){
         this.prefix = prefix;
@@ -11,9 +16,40 @@ public class commands implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
 
+        String[] messageStrings = event.getMessageContent().split("\\|");
+        messageStrings[0] = messageStrings[0].toLowerCase();
+
         //Ping command
-        if(event.getMessageContent().equals(prefix + "ping")){
+        if(messageStrings[0].equals(prefix + "ping")){
             event.getChannel().sendMessage("Pong!");
+        }
+
+        //Flip a coin
+        else if(messageStrings[0].equals(prefix + "flipacoin")){
+            if(rng.nextInt(2) == 0){
+                event.getChannel().sendMessage("Heads");
+            }
+            else{
+                event.getChannel().sendMessage("Tails");
+            }
+        }
+
+        //Meaning of life command
+        else if(messageStrings[0].equals(prefix + "whatisthemeaningoflife")){
+            event.getChannel().sendMessage("The meaning of life is...");
+            event.getChannel().sendMessage("42");
+        }
+
+        //PickRandom
+        else if(messageStrings[0].equals(prefix + "pickrandom")){
+            int selection = rng.nextInt(messageStrings.length -1);
+            selection++;
+            event.getChannel().sendMessage("Selected: " + messageStrings[selection]);
+        }
+
+        //Maths command
+        else if(messageStrings[0].equals(prefix + "domaths")){
+
         }
         else{}
     }
