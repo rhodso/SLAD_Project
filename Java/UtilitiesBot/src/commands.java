3699daa6eb544c750b31faccfb6bed02ab003a93
rhodso <rtrhodes1997@gmail.com>
2019-02-28 16:22:36 +0000

    Got rid of jython because that's effort

--- Java/UtilitiesBot/src/commands.java
+++ Java/UtilitiesBot/src/commands.java
@@ -2,14 +2,12 @@ import java.util.Random;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
-import org.python.util.PythonInterpreter;
 
 public class commands implements MessageCreateListener {
     private String prefix;
     private Random rng = new Random();
-    PythonInterpreter interpreter = new PythonInterpreter();
 
-    public commands(String prefix){
+    public commands(String prefix) {
         this.prefix = prefix;
     }
 
@@ -19,37 +17,38 @@ public class commands implements MessageCreateListener {
         String[] messageStrings = event.getMessageContent().split("\\|");
         messageStrings[0] = messageStrings[0].toLowerCase();
 
-        //Ping command
-        if(messageStrings[0].equals(prefix + "ping")){
+        // Ping command
+        if (messageStrings[0].equals(prefix + "ping")) {
             event.getChannel().sendMessage("Pong!");
         }
 
-        //Flip a coin
-        else if(messageStrings[0].equals(prefix + "flipacoin")){
-            if(rng.nextInt(2) == 0){
+        // Flip a coin
+        else if (messageStrings[0].equals(prefix + "flipacoin")) {
+            if (rng.nextInt(2) == 0) {
                 event.getChannel().sendMessage("Heads");
-            }
-            else{
+            } else {
                 event.getChannel().sendMessage("Tails");
             }
         }
 
-        //Meaning of life command
-        else if(messageStrings[0].equals(prefix + "whatisthemeaningoflife")){
+        // Meaning of life command
+        else if (messageStrings[0].equals(prefix + "whatisthemeaningoflife")) {
             event.getChannel().sendMessage("The meaning of life is...");
             event.getChannel().sendMessage("42");
         }
 
-        //PickRandom
-        else if(messageStrings[0].equals(prefix + "pickrandom")){
-            int selection = rng.nextInt(messageStrings.length -1);
+        // PickRandom
+        else if (messageStrings[0].equals(prefix + "pickrandom")) {
+            int selection = rng.nextInt(messageStrings.length - 1);
             selection++;
             event.getChannel().sendMessage("Selected: " + messageStrings[selection]);
         }
 
-        //Maths command
-        else if(messageStrings[0].equals(prefix + "domaths")){
-
+        // Maths command
+        else if (messageStrings[0].equals(prefix + "domaths")) {
+            PyObject res = interpreter.eval(messageStrings[1]);
+            String result = res.asString();
+            event.getChannel().sendMessage(result);
         }
         else{}
     }
