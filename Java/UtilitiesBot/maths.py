da26abe4041685b5d359a98599f6761f39358189
rhodso <rtrhodes1997@gmail.com>
2019-03-07 16:16:11 +0000

    Updated maths.py

--- Java/UtilitiesBot/maths.py
+++ Java/UtilitiesBot/maths.py
@@ -1,4 +1,14 @@
 import sys
-out = sys.argv[1]
-#f = open('res.txt','w')
-print(str(eval(out)))
+print(str(eval(sys.argv[1])))
+
+#   The Above two lines are so we don't need to write a maths expression parser
+#   A maths expression parser is something like:
+#       Take 1+2 as a string
+#       Get the + operator
+#       Get the 1 as the first opperand
+#       Get the 2 as the second opperand
+#       Perform the operation
+#       Get the result
+#       Output the result
+#   Since this is built into python, let's just write 2 lines of python to do this
+#   which will be far easier than writing the expression parser in java
